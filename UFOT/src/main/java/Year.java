23d3624432d53cc2ab965b14b085406d3c578eda
bhushan43199 <bhushanketanbhai.patel@calbaptist.edu>
2018-10-11 17:54:07 -0700

    jkef

--- UFOT/src/main/java/Year.java
+++
@@ -1,9 +0,0 @@
-import org.apache.logging.log4j.Logger;
-import Classes.Sighting;
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.logging.log4j.LogManager;
-import java.util.concurrent.ConcurrentHashMap;
-
-public class Year {
-}
